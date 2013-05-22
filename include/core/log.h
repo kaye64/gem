77c92cb2b14cf9109fa7507e4af9b0505594bfdd
Kaye T <t.kaye64@gmail.com>
2013-05-22 15:43:21 +0100

    Add logging macros

--- include/core/log.h
+++ include/core/log.h
@@ -9,4 +9,9 @@ void log_warn(const char *tag, const char *fmt, ...);
 void log_info(const char *tag, const char *fmt, ...);
 void log_debug(const char *tag, const char *fmt, ...);
 
+#define ERROR(...) log_error(LOG_TAG, __VA_ARGS__)
+#define WARN(...) log_warn(LOG_TAG, __VA_ARGS__)
+#define INFO(...) log_info(LOG_TAG, __VA_ARGS__)
+#define DEBUG(...) log_debug(LOG_TAG, __VA_ARGS__)
+
 #endif /* _LOG_H_ */
--- init/init.c
+++ init/init.c
@@ -1,12 +1,13 @@
 #include <core/config.h>
 #include <core/log.h>
+#include <runite/cache.h>
 
 #include <stdio.h>
 
 #define LOG_TAG "init"
 
 int main(int argc, char **argv) {
-	log_info(LOG_TAG, "Starting up...");
+	INFO("Starting up...");
 	parse_args(argc, argv);
 	return 0;
-};
+}
