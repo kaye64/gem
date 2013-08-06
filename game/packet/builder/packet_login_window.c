68eba7a7eb57cf8aa2bd62762d60ed77523150cb
Kaye T <t.kaye64@gmail.com>
2013-08-06 01:16:40 +0100

    Fix incorrect flag in login window packet

--- game/packet/builder/packet_login_window.c
+++ game/packet/builder/packet_login_window.c
@@ -14,7 +14,7 @@ packet_t* packet_build_login_window(player_t* player)
 	codec_put8f(&login_window->payload, 0, CODEC_NEGATIVE); // days since recovery changed
 	codec_put16f(&login_window->payload, 0, CODEC_OFS128); // num messages
 	codec_put8(&login_window->payload, 0); // membership warning (??)
-	codec_put32f(&login_window->payload, 0x7F000001, CODEC_MIXED_A); // last ip (127.0.0.1)
+	codec_put32f(&login_window->payload, 0x7F000001, CODEC_MIDDLE_A); // last ip (127.0.0.1)
 	codec_put16(&login_window->payload, 0); // last login
 	return login_window;
 }
